14b96d18e487dfbf578bf6686b3dc29b4912e704
Михайлов <@DESKTOP-84VD5MI>
2023-10-12 20:25:30 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class programm
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число до 1000: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i <= a; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
